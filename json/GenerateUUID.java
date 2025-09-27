a4f4938d404442c2998c155723eb1f9de131a416
liamslane <165489437+liamslane@users.noreply.github.com>
2025-09-27 13:20:53 -0400

    + json files

---
+++ json/GenerateUUID.java
@@ -0,0 +1,8 @@
+import java.util.UUID;
+
+public class GenerateUUID {
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println("UUID=" + uuid.toString() );
+    }
+}
