f89e6acaa33735c95328a96c2de18fe47f85df9f
liamslane <165489437+liamslane@users.noreply.github.com>
2025-10-05 05:41:02 -0400

    + DataWriter (addUser), + driver w/ registration functionality
    Issues 102, 103, 21

--- escapists/src/main/java/com/model/Coding/Data/DataWriter.java
+++ escapists/src/main/java/com/model/Coding/Data/DataWriter.java
@@ -1,32 +1,72 @@
 package com.model.Coding.Data;
+
 import com.model.Coding.User.User;
-import com.model.Coding.Progress.Progress;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class DataWriter {
-  private static DataWriter dataWriter;
+    private static final String USER_FILE = "users.json";
+    private static DataWriter dataWriter;
 
-  private DataWriter() {
+    private DataWriter() { }
 
-  }
-
-  public static DataWriter getInstance() {
-    if (dataWriter == null) {
-      dataWriter = new DataWriter();
+    public static DataWriter getInstance() {
+        if (dataWriter == null) {
+            dataWriter = new DataWriter();
+        }
+        return dataWriter;
     }
-    return dataWriter;
-  }
 
+    public void addUser(User user) {
+        try {
+            File file = new File(USER_FILE);
+            JSONParser parser = new JSONParser();
+            JSONObject root;
+            JSONArray usersArray;
 
-  public void addUser(User user){
-    
-  }
+            // Read file if exists
+            if (file.exists() && file.length() > 0) {
+                FileReader reader = new FileReader(file);
+                root = (JSONObject) parser.parse(reader);
+                reader.close();
 
-  public void updateUser(User user){
+                Object usersObj = root.get("users");
+                if (usersObj instanceof JSONArray) {
+                    usersArray = (JSONArray) usersObj;
+                } else {
+                    usersArray = new JSONArray();
+                    root.put("users", usersArray);
+                }
+            } else {
+                // Create new object if necessary
+                root = new JSONObject();
+                usersArray = new JSONArray();
+                root.put("users", usersArray);
+            }
 
-  }
+            // Create new user object
+            JSONObject newUser = new JSONObject();
+            newUser.put("userName", user.getUserName());
+            newUser.put("password", user.getPassword());
+            newUser.put("currSave", null);
+            newUser.put("saves", new JSONArray());
 
-  public void saveProgress(Progress progress){
-      
-  }
+            // Add new user
+            usersArray.add(newUser);
 
+            // Write back to the file
+            FileWriter writer = new FileWriter(file);
+            writer.write(root.toJSONString());
+            writer.flush();
+            writer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
