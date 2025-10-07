f95dc042716dc92581b5157fe9b0a24a46ca8102
XyuTheAscended <tyler.christian.norman@gmail.com>
2025-10-07 09:12:56 -0400

    Complete #97 and #98

--- escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/EnvironmentProp.java
+++ escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/EnvironmentProp.java
@@ -7,11 +7,16 @@ public class EnvironmentProp {
 
 
 public EnvironmentProp(String name, Item item){
-
+    this.name = name;
+    this.item = item;
+    this.isCollected = false;
 }
 
 public void interact(Inventory playerInventory){
-
+    if (!isCollected) {
+        playerInventory.addItem(item);
+        isCollected = true;
+    }
 }
 
 public Item getItem(){
