1874284e4c9aceb554b4f4cc378b7b1939a7f424
MasonA94 <masonladams@gmail.com>
2025-10-07 08:58:56 -0400

    Completed #94, #95, #96

--- escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/Item.java
+++ escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/Item.java
@@ -6,23 +6,24 @@ public class Item {
     private String description;
 
 
-public Item(int itemId, String name, String description){
+    public Item(int itemId, String name, String description){
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+    }
 
-}
+    public int getItemId(){
+        return itemId;
 
-public int getItemId(){
-    return itemId;
+    }
 
-}
+    public String getName(){
+        return name;
 
-public String getName(){
-    return name;
+    }
 
-}
-
-public String getDescription(){
-    return description;
+    public String getDescription(){
+        return description;
     
-}
-
+    }
 }
