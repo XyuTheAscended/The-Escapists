7e6693df5aa1852296bf18e381792733fbcee65d
MasonA94 <masonladams@gmail.com>
2025-10-12 14:41:02 -0400

    completed #75, #76, #77, #78

--- escapists/src/main/java/com/model/Coding/Gameplay/Timer.java
+++ escapists/src/main/java/com/model/Coding/Gameplay/Timer.java
@@ -3,34 +3,75 @@ package com.model.Coding.Gameplay;
 public class Timer {
     private int startTime;
     private int remainingTime;
-    private boolean isRunning;
+    private boolean isRunning = false;
     private static Timer timer;
+    private Thread timerThread;
 
 
-private Timer(int startTime){
-    this.startTime = startTime;
-}
+    private Timer(int startTime){
+        this.startTime = startTime;
+        this.remainingTime = startTime;
+    }
 
-public static Timer getInstance(){
-    return timer;
+    public static Timer getInstance(int startTime){
+        if(timer == null) {
+            timer = new Timer(startTime);
+        }
+        return timer;
+    }
 
-}
+    public void start(){
+        if (isRunning) return;
+        isRunning = true;
 
-public void start(){
+        timerThread = new Thread(() -> {
+            while (isRunning && remainingTime > 0) {
+                int minutes = remainingTime / 60;
+                int seconds = remainingTime % 60;
+                System.out.printf("Time remaining: %02d:%02d%n", minutes, seconds);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
+                remainingTime--;
+            }
 
-}
+            if (remainingTime == 0) {
+                System.out.println("Time's up!");
+                isRunning = false;
+            }
+        });
+        timerThread.start();
+    }
 
-public void pause(){
+    public void pause(){
+        isRunning = false;
+    }
 
-}
+    public void reset(){
+        isRunning = false;
+        remainingTime = startTime;
+    }
 
-public void reset(){
+    public void resume() {
+        start();
+    }
 
-}
+    public int getRemainingTime(){
+        return remainingTime;
+    }
 
-public int getRemainingTime(){
-    return remainingTime;
+    // temp testing method
+    public static void main(String[] args) throws InterruptedException {
+        Timer timer = getInstance(10);
+        timer.start();
 
-}
+        Thread.sleep(4000);
+        timer.pause();
 
+        Thread.sleep(4000);
+        timer.resume();
+
+    }
 }
