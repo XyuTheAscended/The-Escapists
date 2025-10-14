7fb2f70decc49e156ef6e4951277e00e92f5a35b
XyuTheAscended <tyler.christian.norman@gmail.com>
2025-10-14 09:29:46 -0400

    #108 & #109 Complete

---
+++ escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/ItemPuzzle.java
@@ -0,0 +1,30 @@
+package com.model.Coding.Gameplay.InteractItems;
+
+public class ItemPuzzle extends Puzzle {
+    private Item requiredItem;
+
+    public ItemPuzzle(Item requiredItem){
+        super("", "", "");
+        this.requiredItem = requiredItem;
+    }
+
+    public boolean requiredItem(Item item){
+        if (item == null || requiredItem == null)
+            return false;
+            return item.getItemId() == requiredItem.getItemId();
+        
+    }
+
+    public boolean useItem(Inventory inventory){
+        if (inventory == null || requiredItem == null)
+            return false;
+            
+        for (Item item : inventory.getItems()){
+            if (item.getItemId() == requiredItem.getItemId()){
+                return true;
+            }
+        }  
+    return false;
+    }
+
+}
