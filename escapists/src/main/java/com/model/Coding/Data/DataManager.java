3e07f528564833476c60275730295207d26e27e9
liamslane <165489437+liamslane@users.noreply.github.com>
2025-10-17 11:41:58 -0400

    #28 done, removing duplicate

--- escapists/src/main/java/com/model/Coding/Data/DataManager.java
+++ escapists/src/main/java/com/model/Coding/Data/DataManager.java
@@ -44,9 +44,6 @@ public class DataManager {
     public Progress loadProgress(UUID progressId) {
         return new Progress();
     }
-    public void saveProgress(Progress progress) {
-
-    }
 
     @SuppressWarnings("unchecked")
     private void changeCurrSaveJSON(JSONObject saveJson, Progress progress) {
