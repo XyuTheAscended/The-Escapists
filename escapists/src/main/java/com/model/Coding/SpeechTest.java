197ea00890ecbf8373f8b9211ba8b391ca923039
liamslane <165489437+liamslane@users.noreply.github.com>
2025-10-21 08:53:40 -0400

    + TTS

---
+++ escapists/src/main/java/com/model/Coding/SpeechTest.java
@@ -0,0 +1,10 @@
+package com.model.Coding;
+
+import com.model.Coding.Speech.Speak;
+
+public class SpeechTest {
+    public static void main(String[] args){
+        String message = "Hello, how are you today?";
+        Speak.speak(message);
+    }
+}
