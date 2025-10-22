c8c84ba4407380ef0474c2892f1275d814dc2aaf
Tyler <tcnorman@email.sc.edu>
2025-10-22 11:32:30 -0400

    Javadoc some classes

--- escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/ItemPuzzle.java
+++ escapists/src/main/java/com/model/Coding/Gameplay/InteractItems/ItemPuzzle.java
@@ -2,19 +2,30 @@ package com.model.Coding.Gameplay.InteractItems;
 
 public class ItemPuzzle extends Puzzle {
     private Item requiredItem;
-
+/**
+ * gets required item
+ * @param requiredItem
+ */
     public ItemPuzzle(Item requiredItem){
         super("", "", "");
         this.requiredItem = requiredItem;
     }
-
+/**
+ * Boolean method to make sure user has a required item for the puzzle
+ * @param item
+ * @return
+ */
     public boolean requiredItem(Item item){
         if (item == null || requiredItem == null)
             return false;
             return item.getItemId() == requiredItem.getItemId();
         
     }
-
+/**
+ * Boolean to check if the user can use the item in their inventory
+ * @param inventory
+ * @return
+ */
     public boolean useItem(Inventory inventory){
         if (inventory == null || requiredItem == null)
             return false;
