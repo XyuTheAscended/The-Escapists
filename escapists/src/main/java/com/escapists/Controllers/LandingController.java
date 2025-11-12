df22262050c8e5fc775aa2678f046f1aec305082
Jeffrenzy <jeffeniscool@gmail.com>
2025-11-12 15:34:33 -0500

    Edit styling a little and add landing page

--- escapists/src/main/java/com/escapists/Controllers/LandingController.java
+++ escapists/src/main/java/com/escapists/Controllers/LandingController.java
@@ -1,4 +1,13 @@
 package com.escapists.Controllers;
+import java.io.IOException;
+
+import com.escapists.App;
+
+import javafx.fxml.FXML;
 
 public class LandingController {
+  @FXML
+  private void switchToLogin() throws IOException {
+      App.setRoot("login");
+  }
 }
