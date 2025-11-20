f727cee4d438b14fc1e7fa2f40716e9df98d28fb
MasonA94 <masonladams@gmail.com>
2025-11-20 13:49:57 -0500

    added back button the cutscene. #124

--- escapists/src/main/java/com/escapists/Controllers/CutsceneController.java
+++ escapists/src/main/java/com/escapists/Controllers/CutsceneController.java
@@ -1,12 +1,16 @@
 package com.escapists.Controllers;
+import com.escapists.App;
 import com.model.Coding.Speech.Speak;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import java.io.IOException;
+
 public class CutsceneController {
 
+    public Button btnBack;
     @FXML
     private TextArea cutsceneTextArea;
     @FXML private Button btnContinue;
@@ -37,5 +41,11 @@ public class CutsceneController {
     }
 
     public void btnContinueClicked(ActionEvent actionEvent) {
+        onSceneExit();
+    }
+
+    public void btnBackClicked(ActionEvent actionEvent) throws IOException {
+        onSceneExit();
+        App.setRoot("mainMenu");
     }
 }
